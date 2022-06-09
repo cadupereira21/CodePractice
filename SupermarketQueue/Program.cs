8642d9956b92cc846de448dc5b8464478a675f28
Carlos <carloseduardo2101@gmail.com>
2022-06-09 10:10:30 -0300

    SupermarketQueue completed

--- SupermarketQueue/Program.cs
+++ SupermarketQueue/Program.cs
@@ -2,7 +2,7 @@
 
     public static void Main(){
 
-        int[] queue = new int[]{3, 2, 1};
+        int[] queue = new int[]{5, 3, 4};
         int tills = 1;
 
         Console.WriteLine(QueueTime(queue, tills));
@@ -10,29 +10,31 @@
 
     public static long QueueTime(int[] customers, int n)
     {
+        List<int> auxCustomers = customers.ToList();
         var time = 0;
-        var tills = new int[n];
+        List<int> tills = new List<int>();
 
         for(int i = 0; i < n; i++){
-            tills[i] = customers[i];
-            customers[i] = -1;
+            tills.Add(auxCustomers[i]);
+            auxCustomers.Remove(auxCustomers[i]);
         }
 
-        while(customers[customers.Length-1] != -1){
-            var smallerValueIndex = 0;
+       while(auxCustomers.Count != 0){
+           time += tills.Min();
 
-            for(int i = 0; i<n-1; i++){
-                if(tills[i] < tills[i+1])
-                    smallerValueIndex = i;
-            }
+           for(int i = 0; i < tills.Count; i++){
+               tills[i] -= tills.Min();
 
-            time += smallerValueIndex;
+               if(tills[i] == 0){
+                    tills[i] = auxCustomers.First();
+                    auxCustomers.RemoveAt(0);
+               }
+           }  
+       }
 
-            for(int i = 0; i < n; i++){
-                tills[i] -= tills[smallerValueIndex];
-            }
-
-            
+        while(tills.Sum() > 0){
+            time += tills.Min();
+            tills.Remove(tills.Min());
         }
 
 
