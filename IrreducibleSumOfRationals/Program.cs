9fe8b8912ac488d6c55fd66fabda7bcd232d9898
Carlos <carloseduardo2101@gmail.com>
2022-06-09 14:46:37 -0300

    Create IrreduibleSumOfRationals

---
+++ IrreducibleSumOfRationals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+class Program{
+
+    public static void Main(){
+
+        int[,] aux = {{1,2}, {1, 3}, {1, 4}};
+
+        SumFracts(aux);
+    }
+
+    public static string SumFracts(int[,] l) 
+    {
+        List<int> denominator = new List<int>();
+        List<int> numerator = new List<int>();
+
+        int index = 2;
+        foreach(var value in l){
+            if(index++ % 2 == 0)
+                numerator.Add(value);
+            else
+                denominator.Add(value);
+        }
+
+
+
+        return "";
+    }
+
+}
