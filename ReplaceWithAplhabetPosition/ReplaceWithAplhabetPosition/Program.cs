d16b6b08700e5dec47d8279e196d506d6e0c7787
Carlos <carloseduardo2101@gmail.com>
2022-06-27 13:30:37 -0300

    ReplcWthAlphabetPositn - Created Project

---
+++ ReplaceWithAplhabetPosition/ReplaceWithAplhabetPosition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReplaceWithAplhabetPosition
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }  
+        
+        public static string AlphabetPosition(string text)
+        {
+            return text;
+        }
+    }
+}
