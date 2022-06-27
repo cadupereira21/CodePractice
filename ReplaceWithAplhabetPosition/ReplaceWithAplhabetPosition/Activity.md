9797faef8e8e1cf7e93f9b9b3b685bf3f8ae8ee5
Carlos <carloseduardo2101@gmail.com>
2022-06-27 14:18:47 -0300

    ReplaceWithAlphabetPosition - In Development
     Some tests at Codewars are not correct:
    
     Expected string length 30 but was 32. Strings differ at index 0.
      Expected: "5 16 23 10 16 8 24 19 12 14 16"
      But was:  "0 5 16 23 10 16 8 24 19 12 14 16"
      -----------^

---
+++ ReplaceWithAplhabetPosition/ReplaceWithAplhabetPosition/Activity.md
@@ -0,0 +1,18 @@
+﻿# Replace with alphabet position
+###### C#
+___
+### Enunciado
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+### Exemplo
+`Kata.AlphabetPosition("The sunset sets at twelve o' clock.")`<br>
+
+should return
+
+`20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11` as a string
+___
+_Link para o exercício no Codewars: (https://www.codewars.com/kata/546f922b54af40e1e90001da/train/csharp)_
--- ReplaceWithAplhabetPosition/ReplaceWithAplhabetPosition/Program.cs
+++ ReplaceWithAplhabetPosition/ReplaceWithAplhabetPosition/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ReplaceWithAplhabetPosition
 {
@@ -6,12 +7,42 @@ namespace ReplaceWithAplhabetPosition
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
-        }  
-        
-        public static string AlphabetPosition(string text)
-        {
-            return text;
+            Console.WriteLine(AlphabetPosition("The sunset sets at twelve o' clock."));
         }
+
+        private static string AlphabetPosition(string text)
+        {
+            /*
+             * Para cada char do texto transformado em minusculo
+             * se o valor ASCII do char estiver entre 97 e 122
+             * subtrair o valor por 96
+             * Concatenar valor em uma string
+             */
+            string alphabetPosition = null!;
+            var textToChar = text.ToLower().ToCharArray();
+
+            alphabetPosition += textToChar[0] - 96;
+
+            for (int i = 1; i < textToChar.Length; i++)
+            {
+                if (!(textToChar[i] <= 122 && textToChar[i] >= 97)) continue;
+                alphabetPosition += $" {textToChar[i] - 96}";
+            }
+            
+            return alphabetPosition;
+        }
+
+        // private static int[] ConvertToAscii(string text)
+        // {
+        //     var aux = new int[text.Length];
+        //
+        //     var index = 0;
+        //     foreach (var c in text)
+        //     {
+        //         aux[index++] = c;
+        //     }
+        //     
+        //     return aux;
+        // }
     }
 }
